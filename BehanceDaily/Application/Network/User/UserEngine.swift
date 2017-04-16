c81c87ad410409029949e7f8875f09fdfcc1c713
chenkai <chenkai@xitu.io>
2017-04-16 15:08:11 +0800

    增加用户模块接口封装(空文件)

---
+++ BehanceDaily/Application/Network/User/UserEngine.swift
@@ -0,0 +1,14 @@
+//
+//  UserEngine.swift
+//  BehanceDaily
+//
+//  Created by chenkai on 2017/4/16.
+//  Copyright © 2017年 chenkai. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum User{
+    case SearchUser
+}
